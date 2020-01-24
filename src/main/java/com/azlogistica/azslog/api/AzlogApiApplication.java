e6b072f9765fa27122b36f9439343ae3b12a93e9
Guilherme Reis <guilhermereis14@gmail.com>
2020-01-24 11:38:38 -0300

    Adicionando Api no GitHub

---
+++ src/main/java/com/azlogistica/azslog/api/AzlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.azlogistica.azslog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azlogistica/azslog/api/AzlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azlogistica.azslog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
